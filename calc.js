728e239d326abb3cc7d8999724fad2948305063e
Raheem Ibrahim Ademola <105288689+Hardeymorla@users.noreply.github.com>
2022-06-25 07:01:45 +0100

    calculator

---
+++ calc.js
@@ -0,0 +1,13 @@
+function simpleCalculator() {
+    var first = readint("first:  ");
+    var second = readint("second:  ");
+    var sum = first + second;
+    var subtraction = first - second;
+    var multiplication = first * second;
+    var division = first / second;
+    console.log(sum);
+    console.log(subtraction);
+    console.log(multiplication);
+    console.log(division);
+
+}
