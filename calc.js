ddc26cc4818c019eaa97e15c5907edc83a017ac6
Raheem Ibrahim Ademola <105288689+Hardeymorla@users.noreply.github.com>
2022-07-01 22:33:09 +0100

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,17 @@
+
+var opr = prompt("Operator: ")
+var num1 = parseInt(prompt("number 1: "))
+var num2 = parseInt(prompt("number 2: "))
+
+if (opr == "+") {
+    alert(num1 + num2)
+} else if (opr == "-") {
+    alert(num1 - num2)
+} else if (opr == "*") {
+    alert(num1 * num2)
+} else if (opr == "/") {
+    alert(num1 / num2)
+}
+else {
+    alert("you entered incorrect operator")
+}
